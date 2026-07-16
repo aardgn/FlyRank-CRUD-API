79c1919f6e3ca51056e4a05a1888d45f3e078b28
Arda Gün <ardagun@Arda-MacBook-Air.local>
2026-07-16 21:33:06 +0300

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 app = FastAPI()
 task_db = []
 class TaskCreate(BaseModel):
     title: str
+class TaskUpdate(BaseModel):
+    title: str
+    done: bool
 @app.get("/")
 def read_root():
     return {"name": "Task API", "version": "1.0", "endpoints": ["/task"]}
@@ -12,6 +15,15 @@ def read_root():
 def health_check():
     return {"status": "ok"}
 #Stage2 checkpoint completed
+@app.get("/tasks")
+def get_all_tasks():
+    return task_db
+@app.get("/tasks/{task_id}")
+def get_single_task(task_id: int):
+    for task in task_db:
+        if task["id"] == task_id:
+            return task
+        raise HTTPException(status_code=404, detail="Task not found")
 @app.post("/tasks", status_code=201)
 def task_create(task: TaskCreate):
     if not task.title.strip():
@@ -24,3 +36,20 @@ def task_create(task: TaskCreate):
     }
     task_db.append(new_task)
     return new_task
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task: TaskUpdate):
+        if not task.title.strip():
+            raise HTTPException(status_code=400, detail="Title cannot be empty")
+        for t in task_db:
+            if t["id"] == task_id:
+                t["title"] = task.title
+                t["done"] = task.done
+                return t
+        raise HTTPException(status_code=400, detail="Task not found")
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    for index, t in enumerate(task_db):
+        if t["id"] == task_id:
+            task_db.pop(index)
+            return Response(status_code=204)
+        raise HTTPException(status_code=204, detail="Task not found")
